8b5e9506e9b9a2e64001602ac6aae0ef7ed159ff
WebWizrd8 <superultradev000@gmail.com>
2023-10-22 20:42:35 -0400

    basic auth with jwt

---
+++ src/auth/adminjwt-auth-guard.ts
@@ -0,0 +1,34 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AdminJwtAuthGuard extends AuthGuard('userjwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
+  }
+
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    // info: any,
+    // context: ExecutionContext,
+    // status?: any,
+  ): TUser {
+    if (err || !user || !user.isAdmin) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { UserJwtAuthGuard } from './userjwt-auth-guard';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ session: true }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    { provide: APP_GUARD, useClass: UserJwtAuthGuard },
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
